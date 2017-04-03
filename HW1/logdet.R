520f2eb917deb5bff206b46b61f7cf7d7e903928
xycyx <yxcheng@uw.edu>
2017-04-02 23:58:37 -0700

    Logdet

---
+++ HW1/logdet.R
@@ -0,0 +1,8 @@
+logdet <- function(mat1){
+  # This funciton computes the logarithm of the determinant of a matrix mat1
+  eigen_values <- eigen(mat1)[1]
+  log_values <- log(unlist(eigen_values))
+  log_det <-sum(log_values)
+  return(log_det)
+}
+
