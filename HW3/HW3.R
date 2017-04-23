a5cef661b981765acba3149a7724a71159cb91b4
xycyx <yxcheng@uw.edu>
2017-04-22 23:58:27 -0700

    Hw3

--- HW3/HW3.R
+++ HW3/HW3.R
@@ -9,25 +9,25 @@ getLogisticAIC <- function(response,explanatory,data)
   else {
     #regression with at least one explanatory variable
     deviance = glm(data[,response] ~ as.matrix(data[,as.numeric(explanatory)]),
-                   
+
                    family=binomial(link=logit))$deviance;
-  }  
+  }
   return(deviance+2*(1+length(explanatory)));
-  
+
 }
 
 
 #this is the version of the 'isValidLogistic' function
 #based on Charles Geyers RCDD package
 isValidLogisticRCDD <- function(response,explanatory,data)
-{  
+{
   logisticreg = suppressWarnings(glm(data[,response] ~ as.matrix(data[,as.numeric(explanatory)]),family=binomial(link=logit),x=TRUE));
   tanv = logisticreg$x;
   tanv[data[,response] == 1, ] <- (-tanv[data[,response] == 1, ]);
   vrep = cbind(0, 0, tanv);
   #with exact arithmetic; takes a long time
   #lout = linearity(d2q(vrep), rep = "V");
-  
+
   lout = linearity(vrep, rep = "V");
   return(length(lout)==nrow(data));
 }
@@ -47,7 +47,7 @@ nbdValid <- function(response, A_r, explanatory, data)
   for (addNbd in addNbdSet)
   {
     newSet = c(A_r, addNbd);
-    if (isValidLogisticRCDD(response, newSet, data)) 
+    if (isValidLogisticRCDD(response, newSet, data))
     {
       nbdInd = nbdInd +1 ;
       nbdA_r[[nbdInd]] = newSet;
@@ -57,7 +57,7 @@ nbdValid <- function(response, A_r, explanatory, data)
   for (deleteNbd in A_r)
   {
     newSet = setdiff(A_r, deleteNbd);
-    if (isValidLogisticRCDD(response, newSet, data)) 
+    if (isValidLogisticRCDD(response, newSet, data))
     {
       nbdInd = nbdInd +1 ;
       nbdA_r[[nbdInd]] = newSet;
@@ -78,9 +78,9 @@ modelSelectionMC3 <- function(response, explanatory, data, iterations)
     regValid = isValidLogisticRCDD(response, A_0, data);
   }
   bestModel = A_0;
-  
+
   # iteration
-  
+
   A_r = A_0;
   iter = 0;
   while(iter < iterations)
@@ -93,16 +93,16 @@ modelSelectionMC3 <- function(response, explanatory, data, iterations)
     A_p = unlist(sample(nbd_Ar))
     # step 4
     nbd_Ap = nbdValid(response, A_p, explanatory, data);
-    
+
     # step 5, 6
     AIC_Ap = getLogisticAIC(response, A_p, data);
     AIC_Ar = getLogisticAIC(response, A_r, data);
     AIC_Best = getLogisticAIC(response, bestModel, data);
     p_Ap = - AIC_Ap - log(length(nbd_Ap));
-    p_Ar = - AIC_Ar - log(length(nbd_Ar)); 
-    
+    p_Ar = - AIC_Ar - log(length(nbd_Ar));
+
     # step 7
-    if (p_Ap > p_Ar) 
+    if (p_Ap > p_Ar)
     {
       A_r = A_p;
       if (AIC_Ap < AIC_Best)
@@ -117,7 +117,7 @@ modelSelectionMC3 <- function(response, explanatory, data, iterations)
     else #step 8
     {
       u = runif(1);
-      if (log(u) < (p_Ap - p_Ar)) 
+      if (log(u) < (p_Ap - p_Ar))
       {
         A_r = A_p;
         if (AIC_Ap < AIC_Best)
@@ -135,7 +135,7 @@ modelSelectionMC3 <- function(response, explanatory, data, iterations)
         bestModel;
       }
     }
-    
+
   }
   AICfinal = getLogisticAIC(response, bestModel, data);
   return(list(model = bestModel, AIC = AICfinal));
@@ -148,29 +148,29 @@ main <- function(datafile)
 {
   #read the data
   data = read.table(datafile, header=FALSE);
-  
+
   #the sample size is 148 (number of rows)
   #the explanatory variables are the first 60 columns
   #the last column is the binary response
   response = ncol(data);
   lastPredictor = ncol(data) - 1;
-  
+
   explanatory = c(1:lastPredictor);
   result = modelSelectionMC3(response, explanatory, data, 25);
-  
+
   cat('\n\nbest model is ', result$model);
   cat('\nBest AIC = ', result$AIC);
   # Problem 1
-  # for the small dataset, the result is 
+  # for the small dataset, the result is
 
   for (i in c(1:19))
   {
     result = modelSelectionMC3(response, explanatory, data, 25);
-    
+
     cat('\n\nbest model is ', result$model);
     cat('\nBest AIC = ', result$AIC);
   }
-  
+
 }
 tempdir()
 
@@ -180,29 +180,29 @@ main('534binarydata.txt')
 ##############################################################
 ####### PROBLEM 1
 ##############################################################
-# for small dataset 
+# for small dataset
 best model is  10 2 9 7 8 5 1 4 6 3
 Best AIC =  112.8834
 
 best model is  5 2 7 6 10 3 4 9 1 8
 Best AIC =  112.8834
 The results are very similiar to greedy search.
-# 
+#
 # Problem 2
 # The effiency of this algorithm is not very good, so I got results for 5 instances.
 # The results of MCCC algorithm are different due to its random itialize parameters and sample.
 # For this dataset, the algorithm performs better than greedy search
 # best model is  12 55 27 26 58 35 39 28 30 29 47 44 41 4 50 49 3 56 2 52 53 10 40 20 15 34
 # Best AIC =  83.97523
-# 
+#
 # best model is  15 19 1 44 20 25 3 59 17 21 28 36 30 45
 # Best AIC =  90.71816
-# 
+#
 # best model is  12 8 52 48 53 15 19 45 4 36 57 16 22 31 26 24 38 43 14 21 55 25 30 59 39 9 10 58
 # Best AIC =  131.0038
-# 
+#
 # best model is  18 54 60 6 47 53 25 49 14 59 17 8 1 42 50 38 33 21 55 9 22 39 30 4 24
 # Best AIC =  111.8044
-# 
+#
 # best model is  50 20 8 47 16 37 29 33 13 25 21 17 49 24 54 36 19 2
 # Best AIC =  100.8705
